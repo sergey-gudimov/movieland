38d5ea4ea00782b2f32b8bf6f8090143a71d883d
sergey-gudimov <sergey.gudimov@gmail.com>
2017-12-07 11:00:29 +0200

    b-1 add global exception handler

--- src/main/java/com/gudimov/movieland/util/JsonJacksonConverter.java
+++ src/main/java/com/gudimov/movieland/util/JsonJacksonConverter.java
@@ -12,12 +12,12 @@ public class JsonJacksonConverter {
     private static final Logger LOG = LoggerFactory.getLogger(JsonJacksonConverter.class);
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    public static <T> String itemToJson(T item){
+    public static <T> String itemToJson(T item) {
         try {
             return OBJECT_MAPPER.writeValueAsString(item);
         } catch (JsonProcessingException e) {
             LOG.error("Error convert item to json", e);
-            throw new RuntimeException("Error convert to json",e);
+            throw new RuntimeException("Error convert to json", e);
         }
     }
 }
--- src/main/java/com/gudimov/movieland/web/GlobalExceptionHandler.java
+++ src/main/java/com/gudimov/movieland/web/GlobalExceptionHandler.java
@@ -1,21 +1,31 @@
 package com.gudimov.movieland.web;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import static com.gudimov.movieland.util.JsonJacksonConverter.itemToJson;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseBody
-    public ResponseEntity<?> handlerException(JsonProcessingException e){
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR );
+    public ResponseEntity<?> handlerException(RuntimeException e) {
+        String json = null;
+        try {
+            json = OBJECT_MAPPER.writeValueAsString(e.getMessage());
+        } catch (JsonProcessingException e1) {
+            e1.printStackTrace();
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("JsonHeader", MediaType.APPLICATION_JSON_UTF8_VALUE);
+        return new ResponseEntity<>(json, headers, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 
